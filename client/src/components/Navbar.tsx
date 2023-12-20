f34cc1ac4cdd5fb69e1472335f33e51f57e8811c
Pranjal Gupta <pranjalg1122@gmail.com>
2023-12-20 13:36:11 +0530

    feat: constructed navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,10 +1,15 @@
 import { Sun, Moon } from "react-feather";
-import { Button, variants } from "./Hooks/Button";
+import { Button } from "./Button";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { useEffect } from "react";
+import { SessionDecode } from "../lib/SessionDecode";
+import NewPost from "./NewPost";
+import { UserDetailsContext } from "./Container";
 
 export default function Navbar() {
+  const [newPostInView, setNewPostInView] = useState<boolean>(false);
+
   const [isDarkTheme, setIsDarkTheme] = useState<boolean>(
     localStorage.getItem("isDarkTheme") === "true" ? true : false
   );
@@ -18,27 +23,37 @@ export default function Navbar() {
       localStorage.setItem("isDarkTheme", "false");
     }
   }, [isDarkTheme]);
+
+  const userDetails = useContext(UserDetailsContext);
   return (
-    <nav
-      className={
-        "w-full flex flex-row items-center justify-between px-20 py-4 sticky top-0 " +
-        "bg-white " +
-        "dark:bg-neutral-950"
-      }
-    >
-      <Link
-        to="/"
-        className={variants({
-          variant: "logo",
-        })}
-      >
+    <nav className="w-full flex flex-row items-center justify-between lg:px-20 lg:py-4 py-2 sticky top-0 bg-white dark:bg-neutral-950">
+      <Link to="/">
         <img
-          src={isDarkTheme ? "./logo_dark_theme.png" : "./logo_light_theme.png"}
+          src={isDarkTheme ? "/logo_dark_theme.png" : "/logo_light_theme.png"}
           alt="logo"
           className="w-8"
         />
       </Link>
-      <div className="flex flex-row items-center gap-4">
+      <div className="flex flex-row items-center lg:gap-8 gap-4">
+        {SessionDecode() && window.location.pathname !== "/onboarding" && (
+          <Button
+            onClick={() => {
+              setNewPostInView(true);
+            }}
+            variant="outline"
+          >
+            Create Post
+          </Button>
+        )}
+        {window.location.pathname !== "/" && (
+          <Link to={"/u/" + userDetails?.username}>
+            <img
+              src={userDetails?.avatar}
+              alt="alt"
+              className="lg:h-8 lg:w-8 h-6 w-6 rounded-full"
+            />
+          </Link>
+        )}
         <Button
           onClick={() => {
             setIsDarkTheme(!isDarkTheme);
@@ -48,6 +63,7 @@ export default function Navbar() {
           {isDarkTheme ? <Sun /> : <Moon />}
         </Button>
       </div>
+      {newPostInView && <NewPost setNewPostInView={setNewPostInView} />}
     </nav>
   );
 }
