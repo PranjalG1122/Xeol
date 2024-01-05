2a57f1aaa3a4c9774421a50cf665685cc71f71cf
Pranjal Gupta <pranjalg1122@gmail.com>
2024-01-05 11:58:07 +0530

    feat: added UpdateUser component

---
+++ client/src/components/UpdateUser.tsx
@@ -0,0 +1,5 @@
+export default function UpdateUser() {
+  return (
+    <section className="flex items-start pt-10 px-2 justify-center fixed min-h-screen h-full w-full top-0 left-0 bg-neutral-950 bg-opacity-30 dark:bg-opacity-70"></section>
+  );
+}
