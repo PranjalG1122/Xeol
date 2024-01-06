152abc8cbd5e03111fe3cbb1ed13b4d1444fdb35
Pranjal Gupta <pranjalg1122@gmail.com>
2024-01-06 18:28:51 +0530

    style: make bg-transparent for Button comp

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -9,9 +9,9 @@ export const variants = cva("lg:text-sm text-xs rounded-sm", {
         "focus:outline-none font-medium px-6 py-1 bg-orange-600 text-white hover:bg-orange-500",
       icon: "text-black dark:text-white hover:text-neutral-700 dark:hover:text-neutral-300",
       input:
-        "px-4 py-1 placeholder:text-neutral-500 w-full border focus:outline-none outline-none bg-white border-neutral-300 focus:border-neutral-500 dark:bg-neutral-950 dark:border-neutral-700 dark:focus:border-neutral-500 ",
+        "px-4 py-1 placeholder:text-neutral-500 w-full border focus:outline-none outline-none border-neutral-300 focus:border-neutral-500 bg-transparent dark:border-neutral-700 dark:focus:border-neutral-500 ",
       inputFile:
-        "file:appearance-none file:border-solid file:border file:bg-white file:dark:bg-neutral-950 file:text-black file:dark:text-white file:font-medium file:px-4 file:py-2 file:rounded-sm file:border-neutral-300 file:dark:border-neutral-700 file:shadow-none file:outline-none file:hover:cursor-pointer",
+        "file:appearance-none file:border-solid file:border file:bg-transparent file:text-black file:dark:text-white file:px-4 file:py-1 file:rounded-sm file:border-neutral-300 file:dark:border-neutral-700 file:shadow-none file:outline-none file:hover:cursor-pointer",
       outline:
         "focus:outline-none font-medium px-6 py-1 border border-neutral-300 hover:bg-neutral-100 dark:border-neutral-700 dark:hover:bg-neutral-800 bg-transparent",
     },
