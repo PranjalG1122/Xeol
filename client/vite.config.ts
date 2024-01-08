35d430747bd69090bb4b860ff7c001fea5ff72fa
Pranjal Gupta <pranjalg1122@gmail.com>
2024-01-08 23:05:25 +0530

    feat: updated vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,19 +1,21 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-      "/.*": {
-        target: "/",
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      proxy: {
+        "/api": {
+          target: loadEnv(mode, process.cwd(), "").VITE_SERVER_LINK,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+        "/.*": {
+          target: "/",
+        },
       },
     },
-  },
+  };
 });
