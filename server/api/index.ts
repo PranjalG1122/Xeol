784a6147dd90625900b68dec7d31fe7bc5a36420
Pranjal Gupta <pranjalg1122@gmail.com>
2024-01-12 14:36:03 +0530

    fix: increase limit for avatars express

--- server/api/index.ts
+++ server/api/index.ts
@@ -19,6 +19,17 @@ app.use(express.json());
 app.use(cors());
 app.options("*", cors());
 
+app.use(
+  express.json({
+    limit: "50mb",
+  })
+);
+app.use(
+  express.urlencoded({
+    limit: "50mb",
+  })
+);
+
 app.use("/api/auth", authRouter);
 app.use("/api/session", sessionRouter);
 app.use("/api/onboarding", onboardingRouter);
